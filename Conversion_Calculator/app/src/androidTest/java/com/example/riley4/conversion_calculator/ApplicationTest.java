e8401a3e4569ee033cb03f38577ca6006b3a71d7
Riley Campbell <riley.campbell73@gmail.com>
2014-10-02 15:29:36 -0700

    Initial Upload

---
+++ Conversion_Calculator/app/src/androidTest/java/com/example/riley4/conversion_calculator/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.riley4.conversion_calculator;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
