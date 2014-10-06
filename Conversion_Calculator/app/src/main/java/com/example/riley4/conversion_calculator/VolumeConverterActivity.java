9c285c74310ae5b4cd17074783eebfbe1605d2fc
Riley Campbell <riley.campbell73@gmail.com>
2014-10-06 15:12:20 -0700

    Started coding the volume calculator

--- Conversion_Calculator/app/src/main/java/com/example/riley4/conversion_calculator/VolumeConverterActivity.java
+++ Conversion_Calculator/app/src/main/java/com/example/riley4/conversion_calculator/VolumeConverterActivity.java
@@ -4,17 +4,64 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+//converter app.
+//have constants for everything to convert to and from liter?
 
 
 public class VolumeConverterActivity extends Activity {
+    static final double CuptoLiter = 0.2273045;
+    static final double LitertoTbspUS = 67.628045405;
+    static final double LitertoCup = 4.399384366;
+    String from = "";
+    String to = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volume_converter);
+        final Spinner fromSpinner = (Spinner)findViewById(R.id.spinnerFrom);
+        final Spinner toSpinner = (Spinner)findViewById(R.id.spinnerTo);
+        final EditText fromText = (EditText)findViewById(R.id.EditTextFrom);
+        final EditText toText = (EditText)findViewById(R.id.EditTextTo);
+        fromText.setText("0");
+        toText.setText("0");
+        fromSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                from = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getBaseContext(), from,
+                        Toast.LENGTH_LONG).show();
+                CalculateConversion();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(getBaseContext(), "Please make a selection",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+        toSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                to = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getBaseContext(),to,
+                        Toast.LENGTH_LONG).show();
+                        //CalculateConversion();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(getBaseContext(),"Please make a selection",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,5 +80,25 @@ public class VolumeConverterActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void CalculateConversion(){
+        final EditText fromText = (EditText)findViewById(R.id.EditTextFrom);
+        final EditText toText = (EditText)findViewById(R.id.EditTextTo);
+
+        double value = Double.parseDouble(fromText.getText().toString());
+
+        if (from.equals("Cup[Canada]")){
+            value  = value * CuptoLiter;
+        }
+        if (to.equals("Tablespoon[US]")){
+            value = value * LitertoTbspUS;
+        }
+
+        toText.setText(String.valueOf(value));
+    }
+
+    public void OnCalcClick(View view) {
+        CalculateConversion();
     }
 }
